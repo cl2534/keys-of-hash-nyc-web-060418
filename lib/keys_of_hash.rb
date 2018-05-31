9efd27ec89024d193843ac518f3cbe99c95cf80a
Changmin Lim <chang.lim92@gmail.com>
2018-05-31 04:11:12 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+
 class Hash
   def keys_of(arguments)
-    # code goes here
+    def keys_of(*arguments)
+      result = []
+      self.each do |key, value|
+        if arguments.include?(value)
+          result.push(key) 
+        end
+      end
+    result
+    end 
   end
-end
+end 
